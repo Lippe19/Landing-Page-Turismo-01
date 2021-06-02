5fcc2081a026e853c74ed5cb3a6639fa572158fb
Lippe19 <luizphilippe.sni@gmail.com>
2021-06-02 13:40:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,530 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="fontawesome/all.min.css">
+
+    <link rel="stylesheet" href="css/intlTelInput.min.css">
+
+    <title>Pitangui Beach Resort</title>
+
+    <link rel="icon" href="images/2-Secao/logo.png" sizes="32x32" />
+
+    <style>
+        .btn-primary {
+            color: #4071b8;
+            background-color: #febe5c;
+            border-color: #febe5c;
+        }
+
+        .btn-primary:hover {
+            color: #4071b8;
+            background-color: #ffaf35;
+            border-color: #ffaf35;
+        }
+
+        .primary:not(:disabled):not(.disabled).active,
+        .btn-primary:not(:disabled):not(.disabled):active,
+        .show>.btn-primary.dropdown-toggle {
+            color: #4071b8;
+            background-color: #e29b30;
+            border-color: #e29b30;
+        }
+
+        .btn-primary.focus,
+        .btn-primary:focus {
+            color: #4071b8;
+            background-color: #e29b30;
+            border-color: #e29b30;
+            box-shadow: 0 0 0 0.2rem rgb(38 143 255 / 50%);
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- Banner -->
+    <section class="sc-banner" style="">
+        <div class="container">
+            <div class="sc-margem">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" style="">
+                    <div class="row">
+                        <img src="images/1-Secao/texto.png" style="" class="img-fluid sc1-img1"
+                            alt="Invista em sua casa férias em Natal - RN">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="margin: 0 auto;" class="d-none d-md-block">
+            <div style="" class="sc1-div-img2">
+                <img src="images/1-Secao/planta.png" style="" class="float-end sc1-img2" alt="Planta">
+            </div>
+        </div>
+        <div style="margin: 0 auto;" class="d-block d-md-none">
+            <div style="" class="sc1-div-img2">
+                <img src="images/1-Secao/planta-mobile.png" style="" class="float-end sc1-img2" alt="Planta">
+            </div>
+        </div>
+        <div class="d-block d-md-none">
+            <div style="" class="sc1-div-img3">
+                <a href="#sc2">
+                    <img src="images/1-Secao/seta.png" style="" class="float-end sc1-img3" alt="Seta">
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    <!-- Seção 2 -->
+    <section class="sc-2" id="sc2">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mb-0 mb-md-3">
+                    <img src="images/2-Secao/logo.png" alt="Logo - Pitangui Beach Resort"
+                        class="sc2-logo mx-auto d-block img-fluid">
+                    <p class="font-sc2-titulo text-center mb-0">Bem-vindo ao Pitangui Beach Resort</p>
+                    <p class="font-sc2-texto text-center">
+                        Sua Casa de Férias de frente para o mar com <br class="d-block d-md-none">todas as comodidades
+                        de um resort
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 mb-0 mb-md-2">
+                <img src="images/2-Secao/img.png" alt="Pitangui Beach Resort"
+                    class="mx-auto d-none d-md-block img-fluid">
+                <img src="images/2-Secao/img-mobile.png" alt="Pitangui Beach Resort"
+                    class="mx-auto d-block d-md-none img-fluid">
+            </div>
+            <div class="row align-items-center sc2-padding-seta">
+                <div class="col-12 d-none d-md-block">
+                    <img src="images/2-Secao/seta.png" alt="Seta" class="sc2-img-seta mx-auto d-block img-fluid">
+                </div>
+                <div class="col-12 d-block d-md-none">
+                    <img src="images/7-Secao/seta.png" alt="Seta" class="sc2-img-seta mx-auto d-block img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+    <!-- Seção 3 -->
+    <div class="Sc-3-1">
+        <section class="Sc-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 mb-3 mb-md-4">
+                        <img src="images/3-Secao/titulo.png" style="" class="mx-auto d-block Sc3-img-titulo img-fluid"
+                            alt="Projeto Exclusivo">
+                    </div>
+                </div>
+                <div class="col-12 mb-2">
+                    <img src="images/3-Secao/img.png" style="" class="mx-auto d-none d-md-block Sc3-img img-fluid"
+                        alt="Planta baixa">
+                    <img src="images/3-Secao/img-mobile.png" style=""
+                        class="mx-auto d-block d-md-none Sc3-img img-fluid" alt="Planta baixa">
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="container Sc3-botao-container">
+        <div class="row">
+            <div class="col-12">
+                <a href="#sc9">
+                    <img src="images/3-Secao/botao.png" alt="Quero saber mais" title="Quero saber mais"
+                        class="sc7-botao mx-auto d-block img-fluid">
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Seção 4 -->
+    <section class="Sc-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 my-md-3">
+                    <img src="images/4-Secao/texto.png" alt="São mais de 15 mil m²"
+                        class="Sc4-img-titulo mb-0 mb-md-5 mx-auto d-block img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid container-fluid-grid">
+        <div class="row row-nomargin">
+            <div class="col-12">
+                <div class="container-fluid-grid-item" style="width:100%;height:100%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid d-none d-md-block" src="images/4-Secao/img.jpg" alt="Pitangui Beach Resort">
+                        <img class="img-fluid d-block d-md-none" src="images/4-Secao/img-mobile.jpg" alt="Pitangui Beach Resort">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="sc-5-1A">
+        <section class="sc-5A">
+            <div class="container">
+                <div class="col-12 mb-2">
+                    <img src="images/5-Secao/texto.png" style="" class="mx-auto d-block sc5-img_videoA img-fluid"
+                        alt="Estrutura completa">
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- Seção 5 -->
+    <!-- desktop -->
+    <div class="container-fluid container-fluid-grid d-none d-md-block">
+        <div class="row row-nomargin">
+
+            <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img4.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img7.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img5.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img8.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img3.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img6.jpg" alt="">
+                    </div>
+                </div>
+                <div class="container-fluid-grid-item" style="width:100%;height:33%">
+                    <div class="imagem-sc3-home position-relative">
+                        <img class="img-fluid" src="images/5-Secao/img9.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- mobile -->
+    <div class="container-fluid container-fluid-grid d-block d-md-none">
+        <div class="row row-nomargin">
+                
+                <div class="col-12">
+                    <div class="container-fluid-grid-item" style="width:100%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img1-mobile.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img2.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img3.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img4.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img5.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img6.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img7.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img8.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="container-fluid-grid-item" style="width:50%;height:100%">
+                        <div class="imagem-sc3-home position-relative">
+                            <img class="img-fluid" src="images/5-Secao/img9.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+
+
+
+    <!-- Seção 7 -->
+    <div class="sc-7-1">
+        <section class="sc-7">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 mb-3 mb-md-0">
+                        <img src="images/6-Secao/logo.png" alt="Logo - Pitangui Beach Resort"
+                            class="sc7-logo mx-auto d-block img-fluid">
+                        <p class="font-sc7-titulo text-center mb-0 mb-md-4">
+                            CONHEÇA SUA <font class="font-sc7-tituloN">NOVA CASA DE FÉRIAS</font>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 mb-2">
+                    <a href="https://www.youtube.com/channel/UCM9Sy-mfSWCan27rfWY-dkw" target="_blank">
+                        <img src="images/6-Secao/video.png" style="" class="mx-auto d-block sc7-img_video img-fluid"
+                            alt="Vídeo" title="Vídeo">
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="container sc7-botao-container">
+        <div class="row">
+            <div class="col-12">
+                <a href="#sc9">
+                    <img src="images/6-Secao/botao.png" alt="Quero agora!" title="Quero agora!"
+                        class="sc7-botao mx-auto d-block img-fluid">
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+    <!-- Seção 7 -->
+    <section class="Sc-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 mb-0 mb-md-0 my-0 my-md-5">
+                    <img src="images/7-Secao/titulo.png" alt="Localização privilegiada"
+                        class="Sc7-img-titulo mb-0 mx-auto d-none d-md-block img-fluid">
+                    <img src="images/7-Secao/titulo-mobile.png" alt="Localização privilegiada"
+                        class="Sc7-img-titulo mb-0 mx-auto d-block d-md-none img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid container-fluid-grid">
+        <div class="row row-nomargin">
+            <div class="col-12">
+                <div class="container-fluid-grid-item" style="width:100%;height:100%">
+                    <div class="imagem-sc3-home position-relative">
+                        <a href="https://www.google.com/maps/place/Lagoa+de+Pitangui/@-5.648454,-35.226645,390m/data=!3m1!1e3!4m5!3m4!1s0x7b3a7ecfdb55049:0x587edc9b7d1d9c8d!8m2!3d-5.6468323!4d-35.228927"
+                            target="_blank">
+                            <img class="img-fluid" src="images/7-Secao/mapa.jpg" alt="Mapa">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <section class="sc-seta">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <img src="images/7-Secao/seta.png" alt="Seta" class="sc2-img-seta mx-auto d-block img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+    <!-- Seção 9 -->
+    <section class="secao--2-destino_foz" id="sc9">
+        <div class="imgSc2"></div>
+        <div class="textoSc2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+                        <div role="main" id="landingpage-pitangui-beach-resort-d6db8fe409e877a99973"></div>
+                        <script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
+                        <script type="text/javascript"> new RDStationForms('landingpage-pitangui-beach-resort-d6db8fe409e877a99973', 'UA-115291513-1').createForm();</script>
+
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    <!-- Fim Seção 2 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Rodapé -->
+    <section class="sc-10">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <img src="images/9-Secao/logo.png" style="" alt="" class="my-4 my-md-5 mx-auto d-block img-fluid sc10-img">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="js/telefone/intlTelInput.min.js"></script>
+
+
+
+    <!-- Optional JavaScript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
